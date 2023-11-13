53169b1b2b47fff29e61f5c85e70e7c07adef28a
Hye Chan Lee <112928715+hyechanlee1@users.noreply.github.com>
2023-11-13 03:21:21 -0500

    Update monopolyService.js

--- monopolyService.js
+++ monopolyService.js
@@ -62,7 +62,7 @@ function returnDataOr404(res, data) {
 }
 
 function readHelloMessage(req, res) {
-    res.send('Hello, CS 262 Monopoly service!');
+    res.send('Hello, CS 262 Monopoly service for homework 3!');
 }
 
  function readPlayers(req, res, next) {
